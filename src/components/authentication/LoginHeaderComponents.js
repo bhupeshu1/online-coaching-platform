324f9236d32603eb53bdb9aee01fbfb77e67badf
bhupeshu1 <bhupeshu1@gmail.com>
2024-08-23 17:56:48 +0530

    # login page back and loginHeader components value dynamic all condition

--- src/components/authentication/LoginHeaderComponents.js
+++ src/components/authentication/LoginHeaderComponents.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
-const LoginHeaderComponents = ({ label, cross_icon, back_icon}) => {
+const LoginHeaderComponents = ({back_icon,cross_icon,label}) => {
+  const myParam = useParams();
+  const getLoginLabel=()=>{
+    let loginData = {};
+    switch (myParam.loginType) {
+      case 'student':
+        loginData = {
+          label:
+            'Student Log in',
+        };
+        break;
+      case 'parent':
+        loginData = {
+          label: 'Parent log in',
+        };
+        break;
+      case 'tutor':
+        loginData={
+          label:
+            'Tutor log in',
+        };
+        break;
+      default:
+        loginData={
+          label:
+            'Tutor log in',
+        };
+        break;
+    }
+    return loginData;
+  };
+
+  const { label: resolvedLabel } = getLoginLabel();
+
+  
   return (
     <div className='flex items-center justify-between w-full'>
       <div className='cursor-pointer' onClick={back_icon.props.onClick}>{back_icon} </div>
       <h2 className='text-[#242424] font-bold text-4xl max-md:text-sm'>
-        {label}
+        {resolvedLabel||label}
       </h2>
       <div className='cursor-pointer'>{cross_icon}</div>
     </div>
--- src/utils/mockData.js
+++ src/utils/mockData.js
@@ -7,7 +7,8 @@ export const cartData = [
       'https://cdn.mytutor.co.uk/images/login/parent-mytutor-274x286.png?1723648538000',
     cart_heading: 'I am a Parent',
     cart_description: 'Manage payments or lessons for your child',
-    button_text:"Parent log in"
+    button_text: 'Parent log in',
+    loginType: 'parent',
   },
   {
     id: 178,
@@ -16,7 +17,8 @@ export const cartData = [
     cart_heading: 'I am a Student',
     cart_description:
       'Have lessons, message your tutor or watch your lessons back',
-      button_text:"Student log in "
+    button_text: 'Student log in ',
+    loginType: 'student',
   },
   {
     id: 179,
@@ -24,31 +26,36 @@ export const cartData = [
       'https://cdn.mytutor.co.uk/images/login/tutor-mytutor-330x286.png?1723648513000',
     cart_heading: 'I am a Tutor',
     cart_description: 'Give lessons or manage bookings with your customers',
-    button_text:"Tutor log in"
+    button_text: 'Tutor log in',
+    loginType: 'tutor',
   },
 ];
-export const smallScreenCartData=[
+export const smallScreenCartData = [
   {
-    id:24,
-    cart_small_screen_image:"https://cdn.mytutor.co.uk/images/login/coffee-creditcard-138x150.png?1723648538000",
-    btn_text:"Parent log in"
-},
-{
-  id:23,
-  cart_small_screen_image:"https://cdn.mytutor.co.uk/images/login/book-pen-189x129.png?1723648538000",
-  btn_text:"Student log in"
-},{
-  id:54,
-  cart_small_screen_image:"https://cdn.mytutor.co.uk/images/login/laptop-coffee-189x120.png?1723648000000",
-  btn_text:"Tutor log in"
-  
-}
+    id: 24,
+    cart_small_screen_image:
+      'https://cdn.mytutor.co.uk/images/login/coffee-creditcard-138x150.png?1723648538000',
+    btn_text: 'Parent log in',
+  },
+  {
+    id: 23,
+    cart_small_screen_image:
+      'https://cdn.mytutor.co.uk/images/login/book-pen-189x129.png?1723648538000',
+    btn_text: 'Student log in',
+  },
+  {
+    id: 54,
+    cart_small_screen_image:
+      'https://cdn.mytutor.co.uk/images/login/laptop-coffee-189x120.png?1723648000000',
+    btn_text: 'Tutor log in',
+  },
 ];
-export const parentCartData=[
+export const parentCartData = [
   {
-    id:90,
-    image_url:"https://cdn.mytutor.co.uk/images/login/parent-mytutor-274x286.png?1723648538000",
-    parent_heading:"I am a Parent",
-    parent_des:"Manage payments or lessons for your child"
-  }
+    id: 90,
+    image_url:
+      'https://cdn.mytutor.co.uk/images/login/parent-mytutor-274x286.png?1723648538000',
+    parent_heading: 'I am a Parent',
+    parent_des: 'Manage payments or lessons for your child',
+  },
 ];
